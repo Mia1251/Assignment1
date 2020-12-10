d2250d2be9d7af8a2ba40b03957a83f4f0dacf54
Maria Svensson <75625106+Mia1251@users.noreply.github.com>
2020-12-10 15:30:02 +0100

    Update App.java

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,8 +1,8 @@
 package se.lexicon;
 
 
-public class App 
-{
+public class App {
+    
     public static void main( String[] args ) {
         System.out.println( "Hello" );
         System.out.println("Maria!");
